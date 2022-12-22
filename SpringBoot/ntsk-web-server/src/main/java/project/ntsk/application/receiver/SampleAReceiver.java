9d937eabb124bdbfbce33f67359d863107381cdb
Bito <kai@mail.jtsys.co.jp>
2022-12-22 10:12:46 +0900

    sample up

--- SpringBoot/ntsk-web-server/src/main/java/project/ntsk/application/receiver/SampleAReceiver.java
+++ SpringBoot/ntsk-web-server/src/main/java/project/ntsk/application/receiver/SampleAReceiver.java
@@ -1,6 +1,5 @@
 package project.ntsk.application.receiver;
 
-import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -12,11 +11,11 @@ public class SampleAReceiver extends NtskJmsReceiver {
 
 	private static final String MESSAGE_QUEUE = "TestQue";
 
-	@JmsListener(destination = MESSAGE_QUEUE, containerFactory = "myFactory")
-	public void receive(String message) {
-		log.info("Message received(TestQue): {} ", message);
-
-		log.info("Sleep 5000");
-	}
+//	@JmsListener(destination = MESSAGE_QUEUE, containerFactory = "myFactory")
+//	public void receive(String message) {
+//		log.info("Message received(TestQue): {} ", message);
+//
+//		log.info("Sleep 5000");
+//	}
 
 }
